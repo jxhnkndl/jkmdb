4c80c84352287a5a1cd9235c5754655e21069c9e
jxhnkndl <jkroyston@gmail.com>
2023-06-22 11:51:32 -0400

    create placeholder search results container component

---
+++ src/components/SearchResults.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SearchResults({ heading }) {
+  return (
+    <div>
+      <p className="text-2xl">{heading}</p>
+    </div>
+  );
+}
+
+export default SearchResults;

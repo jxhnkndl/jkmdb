845026f2449d9a42ec0d9162a4d560ea91f395f4
jxhnkndl <jkroyston@gmail.com>
2023-06-24 11:48:53 -0400

    create basic home page layout and temporary function for locally fetching movie data to use during design phase

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import SearchResults from '../components/SearchResults';
+import axios from 'axios';
+
+const TOKEN = process.env.REACT_APP_API_TOKEN;
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+
+const tmdb = axios.create({
+  baseURL: BASE_URL,
+  headers: { Authorization: `Bearer ${TOKEN}` },
+});
 
 function Home() {
+  const [shows, setShows] = useState([]);
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchTrending = async () => {
+      const showData = await tmdb.get(
+        '/trending/tv/day?language=en-US&pages=5'
+      );
+      const movieData = await tmdb.get(
+        '/trending/movie/day?language=en-US&pages=5'
+      );
+
+      setShows(showData.data.results);
+      setMovies(movieData.data.results);
+    };
+
+    // fetchTrending();
+  }, []);
+
   return (
     <div>
-      <p className="text-3xl">HOME PAGE</p>
+      {shows.length >= 0 && movies.length >= 0 && (
+        <section>
+          <div className='my-10'>
+            <p className="text-4xl mb-4">Trending TV</p>
+            <SearchResults />
+          </div>
+          <div className='my-10'>
+            <p className="text-4xl mb-4">Trending Movies</p>
+            <SearchResults />
+          </div>
+        </section>
+      )}
     </div>
   );
 }

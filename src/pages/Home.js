aa801978a378f02f8ef2c3edc00f4a5035b22059
jxhnkndl <jkroyston@gmail.com>
2023-06-26 10:44:39 -0400

    update home page to use movie context and reducer function to make api request and update trending tv and movies state

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,44 +1,45 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useContext } from 'react';
 import SearchResults from '../components/SearchResults';
-import axios from 'axios';
-
-const TOKEN = process.env.REACT_APP_API_TOKEN;
-const BASE_URL = process.env.REACT_APP_BASE_URL;
-
-const tmdb = axios.create({
-  baseURL: BASE_URL,
-  headers: { Authorization: `Bearer ${TOKEN}` },
-});
+import MovieContext from '../context/movie/MovieContext';
+import endpoints from '../utils/apiEndpoints';
 
 function Home() {
-  const [shows, setShows] = useState([]);
-  const [movies, setMovies] = useState([]);
+  const { tvShows, movies, loading, dispatch, fetchData } =
+    useContext(MovieContext);
 
   useEffect(() => {
+    // get trending tv and movie data from tmdb api when home page loads
     const fetchTrending = async () => {
-      const showData = await tmdb.get(
-        '/trending/tv/day?language=en-US&pages=5'
-      );
-      const movieData = await tmdb.get(
-        '/trending/movie/day?language=en-US&pages=5'
-      );
+      // update api loading state to true
+      dispatch({ type: 'SET_LOADING' });
 
-      setShows(showData.data.results);
-      setMovies(movieData.data.results);
+      const tvShows = await fetchData(endpoints.trendingTv);
+      const movies = await fetchData(endpoints.trendingMovies);
+
+      // update state with response data from both api calls
+      dispatch({
+        type: 'GET_TRENDING',
+        payload: {
+          tvShows,
+          movies,
+        },
+      });
     };
 
     // fetchTrending();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
     <div>
-      {shows.length >= 0 && movies.length >= 0 && (
+      {tvShows.length >= 0 && movies.length >= 0 && (
         <section>
-          <div className='my-10'>
+          <div className="my-10">
             <p className="text-4xl mb-4">Trending TV</p>
             <SearchResults />
           </div>
-          <div className='my-10'>
+          <div className="my-10">
             <p className="text-4xl mb-4">Trending Movies</p>
             <SearchResults />
           </div>

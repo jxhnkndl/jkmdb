b685bc2bbd7b8e6b073e3ad4147946816f2a75da
jxhnkndl <jkroyston@gmail.com>
2023-06-26 10:11:15 -0400

    refactor tailwind padding classes to reduce repetition

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
   return (
     <footer className="bg-base-200">
-      <div className="container p-4 md:px-0 lg:px-4">
+      <div className="container p-4 md:max-lg:px-0">
         <p className="text-3xl">FOOTER</p>
       </div>
     </footer>
--- src/layout/Header.js
+++ src/layout/Header.js
@@ -5,7 +5,7 @@ import SearchBar from '../components/SearchBar';
 function Header() {
   return (
     <header className="bg-base-200 shadow-md shadow-base-200 ">
-      <div className="container px-4 md:px-0 lg:px-4 pb-2">
+      <div className="container px-4 md:max-lg:px-0 pb-2">
         <Navbar />
         <SearchBar />
       </div>

754f1b4452b0d34bb84b7c570042ad38542a3c85
jxhnkndl <jkroyston@gmail.com>
2023-06-26 10:43:47 -0400

    import and wrap app component in movie context provider

--- src/App.js
+++ src/App.js
@@ -6,24 +6,27 @@ import TrendingTV from './pages/TrendingTV';
 import TrendingMovies from './pages/TrendingMovies';
 import MovieDetails from './pages/MovieDetails';
 import Footer from './layout/Footer';
+import { MovieProvider } from './context/movie/MovieContext';
 
 function App() {
   return (
-    <Router>
-      <div className="min-h-screen flex flex-col justify-between">
-        <Header />
-        <main className="container px-4 md:max-lg:px-0">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/trending-tv" element={<TrendingTV />} />
-            <Route path="/trending-movies" element={<TrendingMovies />} />
-            <Route path="/search/:title" element={<MovieDetails />} />
-            <Route path="/*" element={<Home />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <MovieProvider>
+      <Router>
+        <div className="min-h-screen flex flex-col justify-between">
+          <Header />
+          <main className="container px-4 md:max-lg:px-0">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/trending-tv" element={<TrendingTV />} />
+              <Route path="/trending-movies" element={<TrendingMovies />} />
+              <Route path="/search/:title" element={<MovieDetails />} />
+              <Route path="/*" element={<Home />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </MovieProvider>
   );
 }
 

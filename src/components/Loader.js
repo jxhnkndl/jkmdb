e73c05dce4358a5207fcad8106ea136d0ed573f5
jxhnkndl <jkroyston@gmail.com>
2023-07-04 17:43:53 -0400

    create animated loader component

---
+++ src/components/Loader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Loader() {
+  return (
+    <div className="flex justify-center">
+      <span className="loading loading-ring w-32"></span>
+    </div>
+  );
+}
+
+export default Loader;

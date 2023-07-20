666a928310ceeb2740090e4614d6f80834fe9fa9
jxhnkndl <jkroyston@gmail.com>
2023-07-20 12:31:51 -0400

    refactor user routes to account for adjustments made to user controller methods

--- server/routes/api/user-routes.js
+++ server/routes/api/user-routes.js
@@ -6,7 +6,7 @@ const {
   searchUsers,
   createUser,
   login,
-  addFriend,
+  followUser,
   saveMovie,
   deleteMovie,
 } = require('../../controllers/user-controller');
@@ -19,7 +19,7 @@ router
   .post(createUser)
   .put(authMiddleware, saveMovie);
 
-router.route('/friends/:friendId').put(authMiddleware, addFriend);
+router.route('/:id/following/:followId').put(authMiddleware, followUser);
 
 router.route('/:movieId').delete(authMiddleware, deleteMovie);
 

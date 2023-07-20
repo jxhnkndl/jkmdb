86749da2ef6c1fea073cb9214d073c6aaf61b76a
jxhnkndl <jkroyston@gmail.com>
2023-07-20 12:30:58 -0400

    refactor auth middleware to directly query db for user submitting token and add user data to req

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const { User } = require('../models');
 
 require('dotenv').config();
 
@@ -7,33 +8,30 @@ const secret = process.env.TOKEN_SECRET;
 const expiration = process.env.TOKEN_EXPIRATION;
 
 module.exports = {
-  // authenticate user for protected routes
-  authMiddleware: function (req, res, next) {
-    // collect token from authorization header
+  authMiddleware: async function (req, res, next) {
     // split token value from "Bearer" in header
     let token = req.headers.authorization.split(' ').pop().trim();
 
-    // if no token
     if (!token) {
       return res.status(401).json({ msg: 'Invalid token' });
     }
 
-    // validate token and extract user data to add to req
+    // use decoded token data to query user and attach user data to req
     try {
-      const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      req.user = data;
-    } catch {
+      const decoded = jwt.verify(token, secret, { maxAge: expiration });
+
+      req.user = await User.findById(decoded.data._id);
+    } catch (err) {
       console.log('Invalid token');
+      console.log(err);
       return res.status(401).json({ msg: 'Invalid token' });
     }
 
     next();
   },
-  // sign new tokens
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
 
-    // configure and sign new token
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };

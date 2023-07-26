b92b7c5f0762e7e1e8aa131cdfcab6075194e034
jxhnkndl <jkroyston@gmail.com>
2023-07-26 12:36:19 -0400

    add form state, input change handler, and basic submit handler to registration page form

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { BiSolidRightArrow } from 'react-icons/bi';
 
 function Register() {
+  const [formData, setFormData] = useState({
+    email: '',
+    username: '',
+    password: '',
+    password2: '',
+  });
+
+  const { email, username, password, password2 } = formData;
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newUser = { email, username, password };
+
+    console.log(newUser);
+  };
+
   return (
     <div className="flex justify-center items-center">
       <div className="w-screen sm:w-1/2 bg-base-200 p-8 rounded-md">
@@ -10,7 +36,7 @@ function Register() {
           <p className="text-4xl mb-4 mr-3 inline">Create Account</p>
           <BiSolidRightArrow className="text-2xl mb-4 inline" />
         </div>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="form-control w-full mb-4">
             <div className="label">
               <div className="label-text">Email</div>
@@ -18,8 +44,9 @@ function Register() {
             <input
               type="email"
               name="email"
-              id="email"
               className="input input-md w-full"
+              value={email}
+              onChange={handleChange}
             />
           </div>
           <div className="form-control w-full mb-4">
@@ -29,8 +56,9 @@ function Register() {
             <input
               type="text"
               name="username"
-              id="username"
-              className="input input-bordered input-md w-full"
+              className="input input-md w-full"
+              value={username}
+              onChange={handleChange}
             />
           </div>
           <div className="form-control w-full mb-4">
@@ -39,9 +67,10 @@ function Register() {
             </div>
             <input
               type="password"
-              name="password1"
-              id="password1"
-              className="input input-bordered input-md w-full"
+              name="password"
+              className="input input-md w-full"
+              value={password}
+              onChange={handleChange}
             />
           </div>
           <div className="form-control w-full mb-10">
@@ -51,14 +80,15 @@ function Register() {
             <input
               type="password"
               name="password2"
-              id="password2"
-              className="input input-bordered input-md w-full"
+              className="input input-md w-full"
+              value={password2}
+              onChange={handleChange}
             />
           </div>
           <button type="submit" className="btn btn-block btn-primary mb-6">
             Sign Up
           </button>
-          <Link to='/login' className='hover:underline'>
+          <Link to="/login" className="hover:underline">
             Already have an account? Sign in instead!
           </Link>
         </form>

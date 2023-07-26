2ba9d6e42124c948d48068c73842455e7e951829
jxhnkndl <jkroyston@gmail.com>
2023-07-26 12:14:37 -0400

    add form components to register and login pages

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import { BiSolidRightArrow } from 'react-icons/bi';
 
 function Login() {
   return (
-    <div>
-      <h1>LOGIN</h1>
+    <div className="flex justify-center items-center">
+      <div className="w-screen sm:w-1/2 bg-base-200 p-8 rounded-md">
+        <p className="text-4xl mb-4 mr-3 inline">Sign In</p>
+        <BiSolidRightArrow className="text-2xl mb-4 inline" />
+        <form>
+          <div className="form-control w-full mb-4">
+            <div className="label">
+              <div className="label-text">Email</div>
+            </div>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className="input input-md w-full"
+            />
+          </div>
+          <div className="form-control w-full mb-12">
+            <div className="label">
+              <div className="label-text">Password</div>
+            </div>
+            <input
+              type="password"
+              name="password1"
+              id="password1"
+              className="input input-bordered input-md w-full"
+            />
+          </div>
+          <button type="submit" className='btn btn-block btn-primary'>Sign In</button>
+        </form>
+      </div>
     </div>
   );
 }
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,9 +1,60 @@
 import React from 'react';
+import { BiSolidRightArrow } from 'react-icons/bi';
 
 function Register() {
   return (
-    <div>
-      <h1>REGISTER USER</h1>
+    <div className="flex justify-center items-center">
+      <div className="w-screen sm:w-1/2 bg-base-200 p-8 rounded-md">
+        <p className="text-4xl mb-4 mr-3 inline">Create Account</p>
+        <BiSolidRightArrow className="text-2xl mb-4 inline" />
+        <form>
+          <div className="form-control w-full mb-4">
+            <div className="label">
+              <div className="label-text">Email</div>
+            </div>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className="input input-md w-full"
+            />
+          </div>
+          <div className="form-control w-full mb-4">
+            <div className="label">
+              <div className="label-text">Username</div>
+            </div>
+            <input
+              type="text"
+              name="username"
+              id="username"
+              className="input input-bordered input-md w-full"
+            />
+          </div>
+          <div className="form-control w-full mb-4">
+            <div className="label">
+              <div className="label-text">Password</div>
+            </div>
+            <input
+              type="password"
+              name="password1"
+              id="password1"
+              className="input input-bordered input-md w-full"
+            />
+          </div>
+          <div className="form-control w-full mb-12">
+            <div className="label">
+              <div className="label-text">Confirm Password</div>
+            </div>
+            <input
+              type="password"
+              name="password2"
+              id="password2"
+              className="input input-bordered input-md w-full"
+            />
+          </div>
+          <button type="submit" className='btn btn-block btn-primary'>Sign Up</button>
+        </form>
+      </div>
     </div>
   );
 }

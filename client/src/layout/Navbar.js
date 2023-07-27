5c1f9e48fab3f0ecb582e68c6c216933a00adb7a
jxhnkndl <jkroyston@gmail.com>
2023-07-27 11:05:33 -0400

    add clear search click functionality to logo to ensure link redirects to trending rather than previous search results

--- client/src/layout/Navbar.js
+++ client/src/layout/Navbar.js
@@ -1,27 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { BiSolidCameraMovie } from 'react-icons/bi';
+import MovieContext from '../context/movie/MovieContext';
+import { CLEAR_SEARCH } from '../context/movie/movieTypes';
 
 function Navbar() {
+  const { dispatch } = useContext(MovieContext);
+
   return (
     <nav className="flex justify-between items-center my-4">
-      <div> 
-        <Link to="/" className="flex items-center text-3xl">
+      <div>
+        <Link
+          to="/"
+          className="flex items-center text-3xl"
+          onClick={() => dispatch({ type: CLEAR_SEARCH })}
+        >
           <BiSolidCameraMovie className="inline mr-1" />
           streamo
         </Link>
       </div>
 
-      <ul className='mt-2'>
+      <ul className="mt-2">
         <li className="inline mr-6">
-          <Link to="/register">
-            Sign Up
-          </Link>
+          <Link to="/register">Sign Up</Link>
         </li>
         <li className="inline">
-          <Link to="/login">
-            Login
-          </Link>
+          <Link to="/login">Login</Link>
         </li>
       </ul>
     </nav>

ea861eb57bb78aa488bc34cf7fa6e603be4b1691
jxhnkndl <jkroyston@gmail.com>
2023-07-28 11:50:04 -0400

    conditionally render navbar links based on login status and add logout functionality to logout link

--- client/src/layout/Navbar.js
+++ client/src/layout/Navbar.js
@@ -2,10 +2,15 @@ import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { BiSolidCameraMovie } from 'react-icons/bi';
 import MovieContext from '../context/movie/MovieContext';
+import AuthContext from '../context/auth/AuthContext';
 import { CLEAR_SEARCH } from '../context/movie/movieTypes';
+import { LOGOUT } from '../context/auth/authTypes';
 
 function Navbar() {
   const { dispatch } = useContext(MovieContext);
+  const { isLoggedIn, dispatch: authDispatch } = useContext(AuthContext);
+
+  console.log(isLoggedIn);
 
   return (
     <nav className="flex justify-between items-center my-4">
@@ -21,12 +26,23 @@ function Navbar() {
       </div>
 
       <ul className="mt-2">
-        <li className="inline mr-6">
-          <Link to="/register">Sign Up</Link>
-        </li>
-        <li className="inline">
-          <Link to="/login">Login</Link>
-        </li>
+        {!isLoggedIn && (
+          <li className="inline mr-6">
+            <Link to="/register">Sign Up</Link>
+          </li>
+        )}
+        {!isLoggedIn && (
+          <li className="inline">
+            <Link to="/login">Login</Link>
+          </li>
+        )}
+        {isLoggedIn && (
+          <li className="inline">
+            <Link to="/" onClick={() => authDispatch({ type: LOGOUT })}>
+              Logout
+            </Link>
+          </li>
+        )}
       </ul>
     </nav>
   );

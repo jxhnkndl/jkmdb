da2a2e26828f8d94778bb6e677f8e858888b6b77
jxhnkndl <jkroyston@gmail.com>
2023-07-29 18:01:03 -0400

    add auth action functions for checking token validity and user login status

--- client/src/context/auth/AuthActions.js
+++ client/src/context/auth/AuthActions.js
@@ -1,11 +1,43 @@
 import axios from 'axios';
+import decode from 'jwt-decode';
+
+export const isTokenValid = (token) => {
+  try {
+    const decodedToken = decode(token);
+
+    // compare expiration with current time in seconds
+    const tokenExpiration = decodedToken.exp;
+    const currentTimestamp = Date.now() / 1000;
+
+    if (tokenExpiration > currentTimestamp) {
+      console.log('TOKEN VALID');
+      return true;
+    } else {
+      console.log('TOKEN EXPIRED');
+      return false;
+    }
+  } catch (err) {
+    console.log(err);
+    return false;
+  }
+};
+
+export const checkToken = () => {
+  const token = localStorage.getItem('token') || null;
+  
+  if (token && isTokenValid(token)) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 export const getMe = async () => {
-  let token = localStorage.getItem('token') || null;
+  let token = checkToken();
 
   if (!token) {
     console.log('User not logged in');
-    return;
+    return false;
   }
 
   try {

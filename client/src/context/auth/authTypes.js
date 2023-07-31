73dcb4e633cef23f06e69f7ce41f2b4fe3a96031
jxhnkndl <jkroyston@gmail.com>
2023-07-31 12:39:56 -0400

    remove auth context, actions, reducer, and provider

--- client/src/context/auth/authTypes.js
+++
@@ -1,6 +0,0 @@
-export const REGISTER_USER = 'REGISTER_USER';
-export const LOGIN = 'LOGIN';
-export const LOGOUT = 'LOGOUT';
-export const SET_USER = 'SET_USER';
-export const SET_AUTH_LOADING_TRUE = 'SET_AUTH_LOADING_TRUE';
-export const SET_AUTH_LOADING_FALSE = 'SET_AUTH_LOADING_FALSE';

7d992b020ad5c564965562ec550e5c4cace789cf
jxhnkndl <jkroyston@gmail.com>
2023-08-04 13:55:45 -0400

    create empty watchlist component

---
+++ client/src/components/EmptyWatchlist.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { BiCameraMovie } from 'react-icons/bi';
+
+function EmptyWatchlist() {
+  return (
+    <div className="flex justify-center items-center min-h-full">
+      <div className="text-center mt-4 lg:mt-0">
+        <p className="text-3xl lg:text-5xl font-bold mb-4">Nothing Saved!</p>
+        <BiCameraMovie className="text-9xl inline mb-4" />
+        <p className='text-1xl'>
+          Check out {' '}
+          <Link className="hover:underline text-primary" to="/trending-tv">
+            Trending TV
+          </Link>{' '}
+          and{' '}
+          <Link className="hover:underline text-primary" to="/trending-movies">
+            Trending Movies
+          </Link>{' '}
+          for popular titles.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default EmptyWatchlist;
